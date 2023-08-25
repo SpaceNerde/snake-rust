0a07651c7e7928a6b5650ff764165fec85cbc82f
Space Nerd <florianpospiech77@gmail.com>
2023-08-25 20:45:59 +0200

    add constants file to store all constants

---
+++ src/constants.rs
@@ -0,0 +1,4 @@
+// window size
+pub const WINDOW_WIDTH: u32 = 500;
+pub const WINDOW_HEIGHT: u32 = 500;
+
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate piston_window;
 use piston_window::*;
 
+mod constants;
+
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("Rust Snake!", (640, 480))
+    let mut window: PistonWindow = WindowSettings::new("Rust Snake!", (constants::WINDOW_WIDTH, constants::WINDOW_HEIGHT))
         .exit_on_esc(true)
         .build()
         .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
