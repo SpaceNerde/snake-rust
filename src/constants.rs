9fb4c1b0b45520a798ec5e2f7954df1aa0b029d5
Space Nerd <florianpospiech77@gmail.com>
2023-08-25 21:44:19 +0200

    change u32 to f64 and add Block Size

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,7 @@
+// default block size
+pub const BLOCK_SIZE: f64 = 32.0;
+
 // window size
-pub const WINDOW_WIDTH: u32 = 500;
-pub const WINDOW_HEIGHT: u32 = 500;
+pub const WINDOW_WIDTH: f64 = BLOCK_SIZE * 16.0;
+pub const WINDOW_HEIGHT: f64 = BLOCK_SIZE * 16.0;
 
