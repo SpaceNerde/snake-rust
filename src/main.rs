d5c24be4e16a5315ce65ef8cf1ac95e44d2d8c29
Space Nerd <florianpospiech77@gmail.com>
2023-08-25 20:36:41 +0200

    Set up basic window settings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate piston_window;
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window: PistonWindow = WindowSettings::new("Rust Snake!", (640, 480))
+        .exit_on_esc(true)
+        .build()
+        .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+    while let Some(e) = window.next() {}
 }
