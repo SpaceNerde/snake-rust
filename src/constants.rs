f4e5fc6972efe2742049b6d8e4e0bdbe2405fe82
Space Nerd <florianpospiech77@gmail.com>
2023-08-25 21:44:56 +0200

    rename BLOCK_SIZE to GRID_BLOCK_SIZE

--- src/constants.rs
+++ src/constants.rs
@@ -1,7 +1,7 @@
 // default block size
-pub const BLOCK_SIZE: f64 = 32.0;
+pub const GRID_BLOCK_SIZE: f64 = 32.0;
 
 // window size
-pub const WINDOW_WIDTH: f64 = BLOCK_SIZE * 16.0;
-pub const WINDOW_HEIGHT: f64 = BLOCK_SIZE * 16.0;
+pub const WINDOW_WIDTH: f64 = GRID_BLOCK_SIZE * 16.0;
+pub const WINDOW_HEIGHT: f64 = GRID_BLOCK_SIZE * 16.0;
 
